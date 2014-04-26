075abba41e61e8a8d61e9e1ceb7b31dc1dfe6c2d
Radoslav Georgiev <rgeorgiev583@gmail.com>
2014-04-26 15:45:44 +0300

    django works here, too

--- unifordummies/urls.py
+++ unifordummies/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',  # nopep8
     # Examples:
     url(r'^$', 'unifordummies.views.index', name='index'),
+    url(r'^test$', 'unifordummies.views.test', name='test'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
