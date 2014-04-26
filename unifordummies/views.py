3be170e2435a56f20417a0f9e7c971d90d87129a
Radoslav Georgiev <rgeorgiev583@gmail.com>
2014-04-26 21:20:07 +0300

    Some code for the Posts and View Post page which, however, is not yet tested. Also: base page for results and error page

--- unifordummies/views.py
+++ unifordummies/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import *
+from default import models
 
-def test(request):
-    return render(request,'unifordummies/test.html', {'categories': '<span style="color: red">Hello World!</span>'})
+def post(request):
+    return render(request, 'unifordummies/post.html', {'post': int(request.GET['pid'])})
+
+def posts(request):
+    if request.method == 'GET':
+        programme_ = request.GET['programme']
+    elif request.method == 'POST':
+        programme_ = request.POST['programme']
+
+    try:
+        page = int(request.GET['page'])
+        posts = Posts.objects.get(programme = programme_)
+    except DoesNotExist:
+        return render(request, 'unifordummies/error.html', {'error': 'Page does not exist'})
+
+    if page * i < len(posts):
+        return render(request, 'unifordummies/posts.html', {'posts': posts[page - 1 * i : 20]})
+    else:
+        return render(request, 'unifordummies/error.html', {'error': 'Page does not exist'})
 
 def index(request):
     return render_to_response('unifordummies/index.html')
