ba8a8390fbd84b4a0e011bde0fd4fe9d0c508761
Artur Balabanov <arturbalabanov97@gmail.com>
2014-04-26 17:13:21 +0300

    Add index view

--- default/views.py
+++ default/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-
 from default import models
 
 
@@ -9,3 +8,8 @@ class ProgrammeListView(ListView):
     model = models.Programme
     templates = 'default/programme_list'
     context_object_name = 'programmes'
+
+
+def index_page_view(request):
+    spec_categories = models.SpecCategory
+    return render('index.html', {spec_categories: spec_categories})
