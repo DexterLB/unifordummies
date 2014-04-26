952204943398b0cbf2eb8f5b9763e032a8f744f9
Artur Balabanov <arturbalabanov97@gmail.com>
2014-04-26 11:27:24 +0300

    Fix bug with South

--- unifordummies/settings.py
+++ unifordummies/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'bower',
-    'South',
 )
 
 MIDDLEWARE_CLASSES = (
