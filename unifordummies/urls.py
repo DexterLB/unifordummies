5749cafa613441f457dedf1ab4a92fb4e4645131
Artur Balabanov <arturbalabanov97@gmail.com>
2014-04-27 16:15:52 +0300

    Fix grid... again and add heading (with a link) to programme posts

--- unifordummies/urls.py
+++ unifordummies/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',  # nopep8
     url(r'^$', 'default.views.index_page_view'),
     url(r'^search/$', 'default.views.search_page_view'),
     url(r'^search/(?P<id>\d+)$', 'default.views.search_cat_view'),
-    url(r'^programme/(?P<programme_id>\d+)$', 'default.views.programme_view'),
+    url(r'^programme/(?P<programme_id>\d+)$', 'default.views.programme_view',
+        name="programme"),
     url(r'^programme/(?P<programme_id>\d+)/(?P<cat_id>\d+)$',
         'default.views.posts_view', name="post_category"),
 
