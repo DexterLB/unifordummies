8c295169d88420dcddf5422541eb810bba6c30ee
Radoslav Georgiev <rgeorgiev583@gmail.com>
2014-04-27 11:09:54 +0300

    merged base.html

--- unifordummies/views.py
+++ unifordummies/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import *
-from default import models
+from functools import reduce
+from django.shortcuts import render
 from django.core.paginator import Paginator
+from default import models
+
+
+class ProgrammeListView(ListView):
+    model = models.Programme
+    templates = 'default/programme_list'
+    context_object_name = 'programmes'
+
+
+def index_page_view(request):
+    spec_cat_cat = models.SpecCategoryCategory.objects.all()
+    spec_cat = models.SpecCategory.objects.all().order_by('name')
+    return render(request, 'default/index.html',
+                  {'spec_cat_cat': spec_cat_cat,
+                   'spec_cat': spec_cat})
+
+
+def search_cat_view(request, id):
+    results = models.Programme.objects.filter(categories__id=id)
+    return render(request, 'default/search.html',
+                  {'results': results})
+
 
 def index(request):
     posts_list = Post.objects.all().order_by('-id')
@@ -21,8 +43,10 @@ def index(request):
         { 'posts' : posts },
         context_instance=RequestContext(request))
 
+
 def post(request):
     return render(request, 'unifordummies/post.html', {'post': int(request.GET['pid'])})
+
 
 def posts(request):
     if request.method == 'GET':
@@ -37,11 +61,12 @@ def posts(request):
         posts = paginator.page(page)
 
         if page * i < len(posts):
-            return render(request, 'unifordummies/posts.html', {'programme': programme_, 'page': page, 'posts': posts[page - 1 * i : 20]})
+            return render(request, 'unifordummies/posts.html', {'programme': programme_, 'posts': posts})
         else:
             return render(request, 'unifordummies/error.html', {'error': 'Page does not exist'})
     except:
         return render(request, 'unifordummies/error.html', {'error': 'Page does not exist'})
+
 
 def index(request):
     return render_to_response('unifordummies/index.html')
