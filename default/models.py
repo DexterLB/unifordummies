d415be9bf39a51a8767a92dfcb327aa942c4a7d9
Artur Balabanov <arturbalabanov97@gmail.com>
2014-04-27 12:28:55 +0300

    Add __str__ to models and pep8-fy the file

--- default/models.py
+++ default/models.py
@@ -1,23 +1,46 @@
 from django.db import models
 
+
 class Uni(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 class SpecCategoryCategory(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
 class SpecCategory(models.Model):
     name = models.CharField(max_length=200)
-    category = models.ForeignKey(SpecCategoryCategory, related_name='categories')
+    category = models.ForeignKey(
+        SpecCategoryCategory, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
 
 class Programme(models.Model):
-    categories = models.ManyToManyField(SpecCategory, related_name='programmes')
+    categories = models.ManyToManyField(
+        SpecCategory, related_name='programmes')
     uni = models.ForeignKey(Uni)
     description = models.TextField()
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
 class PostCategory(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -26,3 +49,7 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     vote = models.IntegerField()
     programme = models.ForeignKey(Programme, related_name='posts')
+
+    # FIXME: Use self.title
+    def __str__(self):
+        return self.text
