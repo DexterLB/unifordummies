24b1ebea29d34f9bb97de42627ef4e68d7639608
Radoslav Georgiev <rgeorgiev583@gmail.com>
2014-04-27 12:13:25 +0300

    merged base.html

--- unifordummies/urls.py
+++ unifordummies/urls.py
@@ -7,15 +7,14 @@ from default.views import ProgrammeListView
 
 
 urlpatterns = patterns('',  # nopep8
-    url(r'^home/$', 'unifordummies.views.index', name='index'),
-    url(r'^post/$', 'unifordummies.views.post', name='post'),
-    url(r'^posts/$', 'unifordummies.views.posts', name='posts'),
-    url(r'^programmes/', ProgrammeListView.as_view(), name='programmes'),
-
     url(r'^$', 'default.views.index_page_view'),
     url(r'^search/$', 'default.views.search_page_view'),
     url(r'^search/(?P<id>\d+)$', 'default.views.search_cat_view'),
-    url(r'^programme/(?P<id>)\d+$', 'default.views.programme_view'),
+    url(r'^programme/(?P<prid>\d+)$', 'default.views.programme_view'),
+    url(r'^programme/(?P<prid>\d+)/(?P<pid>\d+)$', 'default.views.posts_view'),
+    url(r'^programme/(?P<prid>\d+)/post(?P<postid>\d+)$', 'default.views.post_view'),
+    url(r'^programmes/', ProgrammeListView.as_view(), name='programmes'),
 
+    url(r'^home/$', 'unifordummies.views.index_view', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- unifordummies/views.py
+++ unifordummies/views.py
@@ -1,62 +1,58 @@
 from functools import reduce
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.core.paginator import Paginator
 from default import models
 
 
 class ProgrammeListView(ListView):
     model = models.Programme
-    templates = 'default/programme_list'
+    templates = 'unifordummies/programme_list'
     context_object_name = 'programmes'
 
 
 def index_page_view(request):
     spec_cat_cat = models.SpecCategoryCategory.objects.all()
     spec_cat = models.SpecCategory.objects.all().order_by('name')
-    return render(request, 'default/index.html',
-                  {'spec_cat_cat': spec_cat_cat,
-                   'spec_cat': spec_cat})
+    return render(request, 'unifordummies/index.html', {'spec_cat_cat': spec_cat_cat, 'spec_cat': spec_cat})
 
 
 def search_cat_view(request, id):
-    results = models.Programme.objects.filter(categories__id=id)
-    return render(request, 'default/search.html',
-                  {'results': results})
+    results = models.Programme.objects.filter(categories__id = id)
+    return render(request, 'unifordummies/search.html', {'results': results})
 
 
-def index(request):
-    posts_list = Post.objects.all().order_by('-id')
+# MAGIC: Don't touch!
+# When I wrote this, only God and I understood what I was doing.
+# Now, only God knows.
+def search_page_view(request):
+    if request.method == 'POST':
+        req_dict = request.POST
+        chosen_categories_ids = \
+            [int(x[:-4]) for x in req_dict.keys() if 'on' in req_dict[x]]
+        ch_cats = models.SpecCategory.objects.filter(
+            id__in=chosen_categories_ids)
+        results = list(reduce(lambda x, y: set(x) & set(y),
+                       [models.Programme.objects.filter(categories=cat)
+                       for cat in ch_cats]))
+        return render(request, 'unifordummies/search.html',
+                      {'results': results})
 
-    paginator = Paginator(posts_list, 5)
 
+def programme_view(request, id):
+    programme = models.Programme.objects.filter(id = id).order_by('name')
+    return render(request, 'unifordummies/programme.html', {'programme': programme})
+
+
+def post_view(request, postid):
+    post = models.Post.objects.filter(id = postid).order_by('id')
+    return render(request, 'unifordummies/post.html', {'post': post})
+
+
+def posts_view(request, prid, pid):
     try:
-        page = int(request.GET.get('page', '1'))
-    except:
-        page = 1
-
-    try:
-        posts = paginator.page(page)
-    except(EmptyPage, InvalidPage):
-        posts = paginator.page(paginator.num_pages)
-
-    return render_to_response('home/index.html',
-        { 'posts' : posts },
-        context_instance=RequestContext(request))
-
-
-def post(request):
-    return render(request, 'unifordummies/post.html', {'post': int(request.GET['pid'])})
-
-
-def posts(request):
-    if request.method == 'GET':
-        programme_ = request.GET['programme']
-    elif request.method == 'POST':
-        programme_ = request.POST['programme']
-
-    try:
-        posts = Posts.objects.get(programme = programme_).order_by('pkey')
-        page = int(request.GET['p'])
+        posts = Posts.objects.get(programme = prid).order_by('id')
+        page = prid
         paginator = Paginator(posts, 20)
         posts = paginator.page(page)
 
