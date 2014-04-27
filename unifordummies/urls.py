5d2e3c8448bec7dfdd7802192d98d845ed54db22
Radoslav Georgiev <rgeorgiev583@gmail.com>
2014-04-27 12:31:20 +0300

    merged base.html

--- unifordummies/urls.py
+++ unifordummies/urls.py
@@ -7,12 +7,12 @@ from default.views import ProgrammeListView
 
 
 urlpatterns = patterns('',  # nopep8
-    url(r'^$', 'default.views.index_page_view'),
-    url(r'^search/$', 'default.views.search_page_view'),
-    url(r'^search/(?P<id>\d+)$', 'default.views.search_cat_view'),
-    url(r'^programme/(?P<prid>\d+)$', 'default.views.programme_view'),
-    url(r'^programme/(?P<prid>\d+)/(?P<pid>\d+)$', 'default.views.posts_view'),
-    url(r'^programme/(?P<prid>\d+)/post(?P<postid>\d+)$', 'default.views.post_view'),
+    url(r'^$', 'unifordummies.views.index_page_view'),
+    url(r'^search/$', 'unifordummies.views.search_page_view'),
+    url(r'^search/(?P<id>\d+)$', 'unifordummies.views.search_cat_view'),
+    url(r'^programme/(?P<prid>\d+)$', 'unifordummies.views.programme_view'),
+    url(r'^programme/(?P<prid>\d+)/(?P<pid>\d+)$', 'unifordummies.views.posts_view'),
+    url(r'^programme/(?P<prid>\d+)/post(?P<postid>\d+)$', 'unifordummies.views.post_view'),
     url(r'^programmes/', ProgrammeListView.as_view(), name='programmes'),
 
     url(r'^home/$', 'unifordummies.views.index_view', name='index'),
